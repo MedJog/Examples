a391d79f92be09c448f0f1010ebebd08b0512fae
Svetlana <medjog@gmail.com>
2023-03-22 13:42:18 +0400

    add Hello? Sveta

---
+++ Example005_Hello,friend/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username == "Света")
+{
+Console.WriteLine("Ура, это же СВЕТА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
